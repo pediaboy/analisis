a934b39f49334c910c9f98a0d7899cef2e2e3967
pediaboy <thoriqpedia@gmail.com>
2026-05-31 03:24:25 +0700

    Update route.ts

--- app/api/bandar/route.ts
+++ app/api/bandar/route.ts
@@ -3,15 +3,19 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
 
+  const kode =
+    searchParams.get("kode") || "TLKM";
+
   const tradeType =
     searchParams.get("type") || "buy";
 
   const res = await fetch(
-    `https://api.datasectors.com/api/stocks/investors/trade-activity?slug=&time_range=&limit=20&skip=0&trade_type=${tradeType}`,
+    `https://api.datasectors.com/api/stocks/investors/trade-activity?slug=${kode}&limit=20&skip=0&trade_type=${tradeType}`,
     {
       headers: {
         Authorization: `Bearer ${process.env.DATASECTORS_API_KEY}`,
       },
+      cache: "no-store",
     }
   );
 
