8bb5e8631ea40756191d57b6c38eaa15db3a0746
pediaboy <thoriqpedia@gmail.com>
2026-05-31 01:34:19 +0700

    Update route.ts

--- app/api/saham/route.ts
+++ app/api/saham/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: Request) {
   const kode = searchParams.get("kode");
 
   const res = await fetch(
-    `ds_live_73YIBWb3U-lFORtGdYhEdMvCWxhweJvf`,
+    `URL_DATASECTORS_NANTI`,
     {
       headers: {
         Authorization: `Bearer ${process.env.DATASECTORS_API_KEY}`,
