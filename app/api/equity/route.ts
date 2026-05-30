8d130d0e58b71677be57ca6ce81b51e34fa973a2
pediaboy <thoriqpedia@gmail.com>
2026-05-31 02:43:00 +0700

    Update route.ts

--- app/api/equity/route.ts
+++ app/api/equity/route.ts
@@ -1,21 +1,8 @@
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-
-  const kode = searchParams.get("kode");
-
-  const res = await fetch(
-    `https://api.datasectors.com/api/stocks/v2/equities?symbol=${kode}&market=id-id`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.DATASECTORS_API_KEY}`,
-      },
-      cache: "no-store",
-    }
-  );
-
-  const data = await res.json();
-
-  return NextResponse.json(data);
+export async function GET() {
+  return NextResponse.json({
+    success: true,
+    test: "equity hidup",
+  });
 }
