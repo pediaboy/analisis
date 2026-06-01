17c144136d6227c0bfb66c9de313ef94528151ab
pediaboy <thoriqpedia@gmail.com>
2026-06-02 01:41:03 +0700

    redesign: update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+      </head>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
